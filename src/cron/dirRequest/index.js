ab6d884bed1d0d6053a5398e135f8328b0b017d4
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2026-02-20 14:14:46 +0700

    Change cron schedule for Ditbinmas super admin job

--- src/cron/dirRequest/index.js
+++ src/cron/dirRequest/index.js
@@ -93,7 +93,7 @@ const dirRequestCrons = [
     description:
       'Send Ditbinmas super admin daily recap (menus 6/9/34/35) with today period only.',
     schedules: [
-      { cronExpression: '03 21 * * *', handler: () => runDitbinmasSuperAdminDaily(), options: DEFAULT_CRON_OPTIONS },
+      { cronExpression: '45 20 * * *', handler: () => runDitbinmasSuperAdminDaily(), options: DEFAULT_CRON_OPTIONS },
     ],
   },
   // Ditbinmas operator daily recap at 18:12
